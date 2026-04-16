8134bd2b930f4c43492ba3cfec9582da676395e5
hirotoyoshidome <kichiru.3161026@gmail.com>
2026-04-16 19:04:19 +0900

    add install shell

---
+++ install-mac.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# copy .bash_profile and .bashrc to home directory.
+# if exists, backup them to .bash_profile.bak and .bashrc.bak
+# and then copy .bash_profile and .bashrc from this repository.
+if [ -f ~/.bash_profile ]; then
+    mv -f ~/.bash_profile ~/.bash_profile.bak
+fi
+cp -f ./mac/.bash_profile ~/.bash_profile
+
+if [ -f ~/.bashrc ]; then
+    mv -f ~/.bashrc ~/.bashrc.bak
+fi
+cp -f ./mac/.bashrc ~/.bashrc
+
+echo "Installed .bash_profile and .bashrc to home directory."
+exit 0
